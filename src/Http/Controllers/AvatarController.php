59dfc7ed36f2ec218f9200033bfd435090a1acd2
mapo-89 <manuel@postler.de>
2025-07-16 21:15:40 +0200

    ♻️ Refactoring: move avatar file lookup logic into dedicated AvatarService

--- src/Http/Controllers/AvatarController.php
+++ src/Http/Controllers/AvatarController.php
@@ -5,38 +5,31 @@ namespace Mapo89\LaravelAvatarManager\Http\Controllers;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Mapo89\LaravelAvatarManager\Contracts\UserProviderInterface;
+use Mapo89\LaravelAvatarManager\Services\AvatarService;
 
 class AvatarController
 {
     protected $userProvider;
+    protected $avatarService;
 
-    public function __construct(UserProviderInterface $userProvider)
+    public function __construct(UserProviderInterface $userProvider, AvatarService $avatarService)
     {
         $this->userProvider = $userProvider;
+        $this->avatarService = $avatarService;
     }
     public function show($hash)
     {
-        $disk = Storage::disk('public');
-        $basePath = 'avatars/' . $hash;
-
-        // Supported formats, in order of priority
-        $extensions = ['jpg', 'jpeg', 'png', 'gif'];
+        $avatarPath = $this->avatarService->findAvatarByHash($hash);
         
-        // 1. Dynamically find the existing file
-        foreach ($extensions as $ext) {
-            $fullPath = $basePath . '.' . $ext;
-            if ($disk->exists($fullPath)) {
-                return Response::file($disk->path($fullPath));
-            }
+        if ($avatarPath) {
+            return response()->file($this->avatarService->getAvatarFullPath($avatarPath));
         }
 
-         // 2. Fallback to user profile photo
         $user = $this->userProvider->findByEmailHash($hash);
         if ($user && $user->profile_photo_path && Storage::disk('public')->exists($user->profile_photo_path)) {
             return Response::file(Storage::disk('public')->path($user->profile_photo_path));
         }
 
-        // 3. Default avatar
         $defaultPath = public_path(config('avatar-manager.default_avatar', 'vendor/avatar-manager/default.png'));
         return Response::file($defaultPath);
     }
